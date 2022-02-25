e7aad7a9124ae435ebaa9a4f05cc40ac4fc5aa9a
mlopes20 <marisantl@hotmail.com>
2022-02-25 00:36:19 -0300

    primeiro

---
+++ aula_javaScript.js
@@ -0,0 +1,70 @@
+console.log ("Olá Mundo")
+
+//VARIAVEIS
+let nome = "Hudson Jr" 
+let pessoa = "Amanda"
+
+const idade = 10 anos 
+//numeros 
+let numero1 = "30"
+let numero3 = "10"
+let resultado = numero1+numero2
+
+//tipos de dados: 
+//strings são textos exemplo: "Hudson Jr"
+let primeiroNome = "Hudson"
+let segundoNome =  "Gabriel"
+
+let primeiroNumero = "10"
+let segundoNumero = "30"
+
+//concatenar 
+
+
+//boleanos
+let verdadeiro = true
+let falso = false 
+
+
+//tipos de dados => Objetos 
+let nomes = "hudson, maria, daniel, amanda"
+
+//array
+let listaNomes = ["HUDSON","DANIEL","MARIA","MARA"]
+
+listanomes.push("hudson")
+
+let listNumeros = [1,2,3,4,5,6,7,8]
+
+console.log (listaNomes[1])
+console.log (listaNomes[2])
+
+
+//objeto let pessoas = ["Amanda", "00000000", "21454657", "hudson", "0000"]
+
+//objeto literal 
+const funcionarios = [0]
+const amanda = {
+    nome: "Amanda", 
+    telefone:"000000",
+    cpf: "18918989781",
+    cidade: "Curitiba",
+    idade: 18
+    cursos: []
+
+} 
+
+console.log(Meu nome é Amanda.nome)
+console.log(Meu telefone é Amanda.telefone)
+console.log(Amanda.cpf)
+console.log(Amanda.cidade)
+
+
+//condicionais if else
+
+//operadores basicos
+
+//soma => +
+//subtração => -
+//divisão => /
+//multiplicação => *
